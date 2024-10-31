39111207af5cd161066b9e99945cf76187fc79b5
thercistenes <thercistenes1@outlook.com>
2024-10-31 13:18:52 -0300

    8 - Crie uma função que busque por produtos na promoção

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -2,6 +2,21 @@ const stockProducts = require('./data.json');
 
 const getProductsOnSale = () => {
   // Desenvolva seu código dentro dessa função...
+  let produtcSale = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].onSale === true) {
+      produtcSale.push({
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price.toFixed(2)}`,
+        onSale: stockProducts[index].onSale,
+      });
+    }
+  }
+
+  return produtcSale;
 };
+
+console.log(getProductsOnSale());
 
 module.exports = { getProductsOnSale };
