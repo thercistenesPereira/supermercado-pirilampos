005c5eab63aa5ad4c5c2e364480717ffc0da56c4
thercistenes <thercistenes1@outlook.com>
2024-10-31 10:32:09 -0300

    2 - Crie uma função que retorne os nomes dos produtos únicos em estoque

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -2,6 +2,12 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsName = () => {
   // Desenvolva seu código dentro dessa função...
+  let listNames = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    listNames.push(stockProducts[index].productName);
+  }
+
+  return listNames;
 };
 
 module.exports = { getUniqueProductsName };
