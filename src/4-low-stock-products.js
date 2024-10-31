5c5be4bce1913e60d4faa0ec1bf6c7ffc7a69ce3
thercistenes <thercistenes1@outlook.com>
2024-10-31 11:21:56 -0300

    4 - Crie uma função que retorne os produtos com baixo estoque em um formato específico

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -2,6 +2,18 @@ const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+  let lowStock = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].quantityInStock > 0 && stockProducts[index].quantityInStock <= 10) {
+      lowStock
+        .push(`${stockProducts[index].productName}:
+          ${stockProducts[index].quantityInStock} unidades`);
+    }
+  }
+
+  return lowStock;
 };
+
+console.log(getLowStockProducts());
 
 module.exports = { getLowStockProducts };
