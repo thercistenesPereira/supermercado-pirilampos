ffffe156d1ed9267f992d09df61fe3c8d548bcf7
thercistenes <thercistenes1@outlook.com>
2024-10-31 11:12:16 -0300

    3 - Crie uma função que retorne os nomes dos produtos indisponíveis do estoque

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -2,6 +2,15 @@ const stockProducts = require('./data.json');
 
 const getOutOfStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+  let noStock = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].quantityInStock === 0) {
+      noStock.push(stockProducts[index].productName);
+    }
+  }
+
+  return noStock;
 };
 
 module.exports = { getOutOfStockProducts };
