988391534231ac4076f707698d98d3707de994f6
thercistenes <thercistenes1@outlook.com>
2024-10-31 14:34:46 -0300

    10 - Crie uma função que retorne todos os produtos que possuam alguma vitamina em seu valor nutricional

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -1,7 +1,28 @@
 const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
-  // Desenvolva seu código dentro dessa função...
+  let listProduct = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    const product = stockProducts[index];
+    const vitamins = product.nutritionalInfo.vitamins;
+
+    if (vitamins && Object.keys(vitamins).length > 0) {
+      const vitaminsInformation = Object.entries(vitamins).map(
+        ([vitamin, amount]) => `${vitamin} - ${amount}`,
+      );
+
+      listProduct.push({
+        description: product.description,
+        formattedPrice: `R$ ${product.price.toFixed(2)}`,
+        vitaminsInformation,
+      });
+    }
+  }
+
+  return listProduct;
 };
+
+console.log(getProductsRichInVitamin());
 
 module.exports = { getProductsRichInVitamin };
