61751927f63e66b5fb6f44b4dd566661e5365050
thercistenes <thercistenes1@outlook.com>
2024-10-31 14:01:36 -0300

    9 - Crie uma função que retorne todos os produtos com informações sobre alergia ou intolerância

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -1,7 +1,24 @@
 const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
-  // Desenvolva seu código dentro dessa função...
+  const listAlergyOrIntolecance = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    const product = stockProducts[index];
+    const formattedProduct = {
+      description: product.description,
+      formattedPrice: `R$ ${product.price.toFixed(2)}`,
+    };
+
+    if (product.allergyOrIntolerance && product.allergyOrIntolerance.length > 0) {
+      formattedProduct
+        .allergyOrIntoleranceMessage = `Pode conter: ${product.allergyOrIntolerance.join(' ')}`;
+    }
+
+    listAlergyOrIntolecance.push(formattedProduct);
+  }
+
+  return listAlergyOrIntolecance;
 };
 
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
