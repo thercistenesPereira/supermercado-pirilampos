9d421769bdba4f4fea3ac560d5c45e8fb79b37a4
thercistenes <thercistenes1@outlook.com>
2024-10-31 11:29:29 -0300

    5 - Crie uma função que retorne o total de produtos em estoque

--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -2,6 +2,12 @@ const stockProducts = require('./data.json');
 
 const getProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
+  let countProduct = 0;
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    countProduct += stockProducts[index].quantityInStock;
+  }
+
+  return countProduct;
 };
 
 module.exports = { getProductsAmount };
