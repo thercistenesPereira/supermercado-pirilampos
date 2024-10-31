45a55d6b75aacf65c00ccf10347f59bda33f6ca3
thercistenes <thercistenes1@outlook.com>
2024-10-31 13:09:53 -0300

    7 - Crie uma função que busque por produtos de uma marca específica

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,18 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductsByBrand = (brand) => {
+  let searchBrand = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].brand === brand) {
+      searchBrand.push({
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price.toFixed(2)}`,
+      });
+    }
+  }
+
+  return searchBrand;
 };
 
 module.exports = { searchProductsByBrand };
