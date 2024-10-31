ddae6f7c13f1b6062173a5ccb8b14b27d7325ec1
thercistenes <thercistenes1@outlook.com>
2024-10-31 11:48:40 -0300

    6 - Crie uma função que busque um produto pelo nome

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,21 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
+const searchProductByName = (name) => {
   // Desenvolva seu código dentro dessa função...
+  if (!name) {
+    return null;
+  }
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].productName === name) {
+      return {
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price.toFixed(2)}`,
+      };
+    }
+  }
+
+  return null;
 };
 
 module.exports = { searchProductByName };
